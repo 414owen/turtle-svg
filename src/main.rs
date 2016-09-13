0dcb3b8c89748815fe77b26f714295c8ba5ffc4f
Owen Shepherd <414owen@gmail.com>
2016-09-13 23:20:47 +0100

    Got some datatypes going, some input matching. This show is on the road.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::prelude::*;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Initialising turtle.");
+    let stdin = io::stdin();
+    let mut line_num = 0;
+    for line in stdin.lock().lines() {
+        line_num = line_num + 1;
+        match &line.unwrap()[..] {
+            "hi" => println!("MATCH!"),
+            bad_input => println!("Invalid input on line {}:\n{}", line_num, bad_input),
+        }
+    }
+}
+
+struct Point(i32,i32);
+
+struct Pen {
+    thickness: i32,
+    color: String,
+}
+
+struct Turtle {
+    position: Point,
+    breaing: i32,
+    pen: Pen,
 }
