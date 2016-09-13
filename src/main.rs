7544a2fda8bdc85dec9704fc09fd6b26e5714c8b
Owen Shepherd <414owen@gmail.com>
2016-09-14 00:30:46 +0100

    Added commands for editind pen size and up/down position

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,59 @@
 use std::io::prelude::*;
 use std::io;
+use std::env;
 
 fn main() {
-    println!("Initialising turtle.");
     let stdin = io::stdin();
     let mut line_num = 0;
+    use std::fs::File;
+    let width = 500;
+    let height = 500;
+
+    let mut turtle: Turtle = Turtle {
+        position: Point { x: width / 2, y: height / 2 },
+        bearing: 0,
+        pen: Pen {
+            thickness: 1,
+            color: "#000".to_string(),
+            down: true
+        }
+    };
+
+    println!("<svg width={} height={}>", width, height);
     for line in stdin.lock().lines() {
         line_num = line_num + 1;
-        match &line.unwrap()[..] {
-            "hi" => println!("MATCH!"),
-            bad_input => println!("Invalid input on line {}:\n{}", line_num, bad_input),
+        let lineuw = line.unwrap();
+        let mut elems = lineuw.split(' ');
+        match elems.next().unwrap() {
+            "pu" => turtle.pen.down = false,
+            "pd" => turtle.pen.down = true,
+            "ps" => turtle.pen.thickness = elems.next().unwrap().parse::<i32>().unwrap(),
+            bad_input => {
+                println!("Invalid input on line {}:\n{}", line_num, bad_input);
+                std::process::exit(0);
+            },
         }
+        println!("{:?}", turtle);
     }
+    println!("</svg>");
 }
 
-struct Point(i32,i32);
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
 
+#[derive(Debug)]
 struct Pen {
     thickness: i32,
     color: String,
+    down: bool,
 }
 
+#[derive(Debug)]
 struct Turtle {
     position: Point,
-    breaing: i32,
+    bearing: i32,
     pen: Pen,
 }
