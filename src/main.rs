b756ff051270ff7adaab168e74e10f30d4c14224
Owen Shepherd <414owen@gmail.com>
2016-09-14 13:04:48 +0100

    Lines now work!

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::io::prelude::*;
 use std::io;
 use std::env;
+use std::f64::consts::PI;
 
 fn main() {
     let stdin = io::stdin();
     let mut line_num = 0;
-    use std::fs::File;
     let width = 500;
     let height = 500;
-
     let mut turtle: Turtle = Turtle {
         position: Point { x: width / 2, y: height / 2 },
-        bearing: 0,
+        bearing: 0.0f64,
         pen: Pen {
             thickness: 1,
             color: "#000".to_string(),
@@ -19,23 +18,56 @@ fn main() {
         }
     };
 
-    println!("<svg width={} height={}>", width, height);
+    let mut outPort = io::stdout();
+    let mut lastCmd = "";
+    println!("<svg width={} height={} xmlns='http://www.w3.org/2000/svg'>", width, height);
     for line in stdin.lock().lines() {
         line_num = line_num + 1;
         let lineuw = line.unwrap();
         let mut elems = lineuw.split(' ');
-        match elems.next().unwrap() {
+        let cmd = elems.next().unwrap();
+        match cmd {
+            "fd" => {
+                let start = Point { x: turtle.position.x, y: turtle.position.y };
+                turtle.position = newPos(&turtle.position, turtle.bearing, elems.next().unwrap().parse::<i32>().unwrap());
+                let end = &turtle.position;
+                if turtle.pen.down {
+                    write!(
+                        outPort,
+                        "<line x1='{}' y1='{}' x2='{}' y2='{} stroke='{}' stroke-width='{}'>\n",
+                        start.x, start.y, end.x, end.y, turtle.pen.color, turtle.pen.thickness
+                    );
+                }
+            },
             "pu" => turtle.pen.down = false,
             "pd" => turtle.pen.down = true,
             "ps" => turtle.pen.thickness = elems.next().unwrap().parse::<i32>().unwrap(),
-            bad_input => {
-                println!("Invalid input on line {}:\n{}", line_num, bad_input);
+            "pc" => turtle.pen.color = elems.next().unwrap().to_string(),
+            _ => {
+                write!(outPort, "Invalid input on line {}:\n{}\n", line_num, lineuw);
+                outPort.flush();
                 std::process::exit(0);
             },
         }
-        println!("{:?}", turtle);
+        write!(outPort, "{:?}\n", turtle);
     }
-    println!("</svg>");
+    write!(outPort, "</svg>\n");
+    outPort.flush();
+}
+
+fn newPos(point: &Point, bearing: f64, amount: i32) -> Point {
+    let dir = bearing / 180.0f64 * PI;
+    Point { x: point.x + ((amount as f64 * dir.cos()) as i32), y: point.y + ((amount as f64 * dir.sin()) as i32) }
+}
+
+enum Cmd {
+    penUp,
+    penDown,
+    penSize,
+    penColor,
+    forward,
+    leftTurn,
+    rightTurn,
 }
 
 #[derive(Debug)]
@@ -54,6 +86,7 @@ struct Pen {
 #[derive(Debug)]
 struct Turtle {
     position: Point,
-    bearing: i32,
+    bearing: f64,
     pen: Pen,
 }
+
